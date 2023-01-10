dc30cf36ba3ad6514f418caa622ffe144fd00597
subedisagar12 <subedisagar12@gmail.com>
2023-01-10 09:51:51 +0545

    Home page change

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>This is home page</h1>
       <Link to="/about">Go to About</Link>
     </>
   );
